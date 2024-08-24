3949a28bbe5ae5ab255e4eec24489c27614923d4
HaMZAAsif043 <asifnaseer043@gmail.com>
2024-08-24 16:39:09 +0500

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './routes/App'
-import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './routes/App';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import CreatePost, { createPostAction } from './components/CreatePost';
 import PostList from './components/PostList';
 
+// Define your base URL here
+const basename = '/Social_MediaAPP';
+
+// You can use a custom base URL in the routes
 const router = createBrowserRouter([
   {
-    path: '/', element: <App />, children: [
-      { path: '/post-list', element: <PostList />, },
-      { path: "/create-post", element: <CreatePost />, action: createPostAction }]
+    path: `${basename}/`, element: <App />, children: [
+      { path: `${basename}/post-list`, element: <PostList /> },
+      { path: `${basename}/create-post`, element: <CreatePost />, action: createPostAction }
+    ]
   },
-  ,
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
